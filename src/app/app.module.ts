56257a0677d96ad5af6db31b649672f5cd5591d0
Brian D'Mello <brian_caa@yahoo.com>
2019-07-25 16:33:13 -0700

    added material styling to buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,16 @@ import { FormsModule } from '@angular/forms';
 import { Ng2CarouselamosModule } from 'ng2-carouselamos';
 
 import {A11yModule} from '@angular/cdk/a11y';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+// import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FeaturedItemsComponent } from './featured-items/featured-items.component';
 import { SelectedItemComponent } from './selected-item/selected-item.component';
 import { ItemTagsComponent } from './item-tags/item-tags.component';
 import { ItemNavigatorComponent } from './item-navigator/item-navigator.component';
 import { CartComponent } from './cart/cart.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { FocusTrapFactory, FocusMonitor, ListKeyManager} from '@angular/cdk/a11y'
 
 
 @NgModule({
@@ -38,7 +41,10 @@ import { CartComponent } from './cart/cart.component';
     FormsModule,
     Ng2CarouselamosModule,
     A11yModule,
-    NoopAnimationsModule
+    // NoopAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    BrowserAnimationsModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
